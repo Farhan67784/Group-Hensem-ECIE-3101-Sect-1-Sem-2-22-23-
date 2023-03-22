b64a90684e9094b656a3862ce0ec1143a40cd071
Farhan67784 <127961694+Farhan67784@users.noreply.github.com>
2023-03-22 13:04:55 +0800

    Update greatest common div.py
    Update comments on coding

--- greatest common div.py
+++ greatest common div.py
@@ -3,7 +3,7 @@
 #  ABDULLAH FARHAN BIN ABD NASIR (2013219)
 #  MUHAMMAD NABIL ARRASYID (2011093)
 #  IMAN IRMANISYA BIN BAKRI (1715123)
-#
+#  Ahmad Zal Hasmi Bin Ahmad Jafry (2012991)
 #
 #  Practice for Chapter 0
 # This code is to calculate the greatest common divisor of two numbers inputed by the user
